3158f6bd92b4f5c8a3d02ef71cdf5cac2c8c21a9
Jon Phenow <j.phenow@gmail.com>
2011-09-05 10:08:36 -0700

    Cleaned up the JS to one event handler for both buttons

--- app/webroot/js/game.js
+++ app/webroot/js/game.js
@@ -3,11 +3,23 @@ $(document).ready(function() {
 	 * Check what status the arrow is currently at based on class
 	 * If it was originally unclicked, set to clicked and popup dialog
 	 * asking if they really want to attempt an action
+	 *
+	 * Slightly sluggish since combining the two into one click handler
 	 */
-	$('.arrow').click( function(e){
-		if( $(this).hasClass('unclicked') ){
-			$(this).removeClass('unclicked');
-			$(this).addClass('clicked');
+	$('.arrow, .bag').click( function(e){
+		var selected, unselected, form_id; //Give us access to classes for certain scenarios
+		if( $(this).attr('class').indexOf('arrow') != -1 ){
+			selected = 'clicked';
+			unselected = 'unclicked';
+			form_id = '#GameVote';
+		} else if( $(this).attr('class').indexOf('bag') != -1){
+			selected = 'owned';
+			unselected = 'not_owned';
+			form_id = '#GameMoveOwned';
+		}
+		if( $(this).hasClass(unselected) ){
+			$(this).removeClass(unselected);
+			$(this).addClass(selected);
 			var original_clicked = $(this); // Need for deeper interaction later
 			$('#ays').dialog({
 				modal: true,
@@ -19,7 +31,7 @@ $(document).ready(function() {
 					"Submit": function() {
 						// Set to game_id contents, a hidden class
 						var game_id = $(original_clicked).siblings(".game_id").html();
-						$('#GameVote').val(game_id);
+						$(form_id).val(game_id);
 						$('form#GameIndexForm').submit();
 					},
 					"Cancel": function() {
@@ -30,42 +42,8 @@ $(document).ready(function() {
 				},
 			});
 		} else{ // For the fake click above
-			$(this).removeClass('clicked');
-			$(this).addClass('unclicked');
-		}
-	});
-	/* Same as arrow check with some tweaks to check the status
-	 * of bag and submit accordingly. 
-	 * TODO combine the two into one, more manageable method
-	 */
-	$('.bag').click( function(e){
-		if( $(this).hasClass('not_owned') ){
-			$(this).removeClass('not_owned');
-			$(this).addClass('owned');
-			var original_clicked = $(this); // Need for deeper interaction later
-			$('#ays').dialog({
-				modal: true,
-				resizable: false,
-				title: "Confirm",
-				show: 'drop',
-				hide: 'drop',
-				buttons: {
-					"Submit": function() {
-						// Set to game_id contents, a hidden class
-						var game_id = $(original_clicked).siblings(".game_id").html();
-						$('#GameMoveOwned').val(game_id);
-						$('form#GameIndexForm').submit();
-					},
-					"Cancel": function() {
-						$('#ays').dialog('close');
-						// perform a fake click to set to button back to original state
-						$(original_clicked).click();
-					}
-				},
-			});
-		} else{// For the fake click above
-			$(this).removeClass('owned');
-			$(this).addClass('not_owned');
+			$(this).removeClass(selected);
+			$(this).addClass(unselected);
 		}
 	});
 });
